2b1f7aaf966a38f59cdfc1eb39969073dbf8f47e
kotetuco <paprapapra@gmail.com>
2021-10-14 00:00:22 +0900

    Add Xcode project.

---
+++ iOS15-SwiftUI-Sample/iOS15-SwiftUI-Sample/iOS15_SwiftUI_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15_SwiftUI_SampleApp.swift
+//  iOS15-SwiftUI-Sample
+//
+//  Created by kotetu on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct iOS15_SwiftUI_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
